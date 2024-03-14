352eaa9b01c30a8d82c36749c9188ef5ae595aa0
chinnamsaireddy1369 <chinnamsaireddy1369@gmail.com>
2024-03-14 12:51:08 -0700

    added local storage & fixed issues pointed yesterday

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
+const getLocalList=()=>{
+  const storedTodos = localStorage.getItem('todos');
+  // console.log(storedTodos);
+   if (storedTodos!=null) {
+     const newTod = JSON.parse(storedTodos);
+     //console.log(newTod);
+     return newTod;
+   }else{
+    return [];
+   }
+}
 function TodoList() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(getLocalList());
   const [inputValue, setInputValue] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+   // console.log(todos);
+  }, [todos]);
 
   function handleDeleteValue(index) {
     const newTodo = [...todos];
@@ -11,19 +26,22 @@ function TodoList() {
   }
 
   function handleChangeValue(e) {
+   // if(e.target.value!==""){
     setInputValue(e.target.value);
+   // }
   }
 
   function handleSubmitValue(e) {
     e.preventDefault();
+    if(inputValue!==""){
     setTodos([...todos, inputValue]);
     setInputValue('');
+    }
   }
 
   function moveItemUp(index) {
     if (index === 0) return;
     const newTodos = [...todos];
-    //swapping 
     const temp = newTodos[index - 1];
     newTodos[index - 1] = newTodos[index];
     newTodos[index] = temp;
@@ -42,20 +60,20 @@ function TodoList() {
   return (
     <div>
       <h1>Daily Todo List</h1>
-      <form>
-        <input type='text'   value={inputValue} onChange={handleChangeValue}/>
-        <button onClick={handleSubmitValue} > AddTodo </button>
+      <form onSubmit={handleSubmitValue}>
+        <input type='text' value={inputValue} onChange={handleChangeValue} placeholder="Enter your todo here"/>
+        <button type="submit">Add Todo</button>
       </form>
-      <ul>
+      <ol>
         {todos.map((todo, index) => (
           <li key={index}>
-            {todo}
+            {index + 1}. {todo}
             <button onClick={() => handleDeleteValue(index)}>Delete</button>
             <button onClick={() => moveItemUp(index)}>Move Up</button>
             <button onClick={() => moveItemDown(index)}>Move Down</button>
           </li>
         ))}
-      </ul>
+      </ol>
     </div>
   );
 }
